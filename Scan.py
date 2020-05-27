10def02d8d06db4dd52f523fb7e98d4c8b675efc
polosec <690279211@qq.com>
2020-05-27 17:58:32 +0800

    first commit

---
+++ Scan.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+# @Time    : 2020/5/25 22:13
+# @Author  : PoLoSec.
+# @File    : Scan.py
+# @Software: PyCharm
+import json
+import  nmap
+import os
+import threading
+nm=nmap.PortScanner()
+
+class Scanner():
+    def __init__(self,args):
+        self.delay=args.delay
+        self.threadnum=args.threadnum
+        self.url=args.url
+        self.thread_max = threading.BoundedSemaphore(self.threadnum)
+        self.threads=[]
+    def Ms(self,ip):
+        if os.path.exists('res.json'):
+            os.remove('res.json')
+        cmd = " masscan.exe " + ip + " -p 1-80 -oJ res.json --rate 10000"
+        os.system(cmd)
+        with open('res.json', 'r') as f:
+            data = json.load(f)
+            lenth = len(data)
+            for i in range(lenth):
+                ip = data[i]['ip']
+                port = data[i]['ports'][0]['port']  # 拿到开放端口号
+                self.thread_max.acquire()
+                t = threading.Thread(target=self.nmapscan, args=(ip, port))
+                self.threads.append(t)
+                t.start()
+            for t in self.threads:
+                t.join()
+    def save(self,strings):
+
+        with open(self.url+'.txt','a') as f1:
+            f1.write(str(strings))
+            f1.close()
+    def nmapscan(self,host, port):
+        res = nm.scan(host, arguments='-sV -p' + str(
+            port) + ' -script-args http.useragent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36"')
+        for host, result in res['scan'].items():
+            if (result['status']['state'] == 'up'):
+                try:
+                    for port in result['tcp']:
+                        try:
+                            strings='Host :%s TCP Port:%s,Status:%s,Name:%s,Product:%s.\n' % (
+                            host,port, result['tcp'][port]['state'], result['tcp'][port]['name'],
+                            result['tcp'][port]['product'])
+                            print(strings)
+                            self.save(strings)
+                        except:
+                            pass
+                except:
+                    pass
+                try:
+                    for port in result['udp']:
+                        try:
+                            print('UDP Port:%s,Status:%s,Name:%s,Product:%s.' % (
+                            port, result['udp'][port]['state'], result['udp'][port]['name'],
+                            result['udp'][port]['product']))
+                        except:
+                            pass
+                except:
+                    pass
+        self.thread_max.release()
---
+++ quickscan.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+# @Time    : 2020/5/25 17:44
+# @Author  : PoLoSec.
+# @File    : quickscan.py
+# @Software: PyCharm
+import time
+import argparse
+from Scan import  Scanner
+import  re
+def getIp(domain):
+    import socket
+    myaddr = socket.getaddrinfo(domain,'http')[0][4][0]
+    return  myaddr
+def main():
+    parser=argparse.ArgumentParser()
+    parser.add_argument('url')
+    parser.add_argument('-d',dest='delay',type=float,default=0)
+    parser.add_argument('-t',dest='threadnum',default=10,type=int)
+    args=parser.parse_args()
+    urls=re.split(',',args.url)#这是待进行解析的URL
+    ips=list()
+    for url in urls:
+        ipaddr=getIp(url)
+        ips.append(ipaddr)
+    ips=','.join(ips)#解析后的IP地址，通过","连接
+    scanner=Scanner(args)
+    scanner.Ms(ips)
+if __name__=='__main__':
+    b=time.time()
+    main()
+    f=time.time()
+    print("任务执行完成，总用时%.2f秒"%(f-b))
